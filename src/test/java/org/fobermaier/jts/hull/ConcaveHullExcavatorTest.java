db9b5a97fe9a24423eda3e45ac4185919d7887a4
Felix Obermaier <felix.obermaier@netcologne.de>
2021-03-19 14:10:50 +0100

    Initial commit
    Imported and adapted code from /locationtech/jts/pull/427
    
    Signed-off-by: Felix Obermaier <felix.obermaier@netcologne.de>

---
+++ src/test/java/org/fobermaier/jts/hull/ConcaveHullExcavatorTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2016 Vivid Solutions.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+package org.fobermaier.jts.hull;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.fobermaier.jts.TestFiles;
+import org.fobermaier.jts.algorithm.ConcaveHullExcavator;
+import org.locationtech.jts.algorithm.ConvexHull;
+import org.locationtech.jts.algorithm.match.HausdorffSimilarityMeasure;
+import org.locationtech.jts.geom.Geometry;
+
+import org.locationtech.jts.io.Ordinate;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.locationtech.jts.io.WKTFileReader;
+import org.locationtech.jts.io.WKTWriter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ConcaveHullExcavatorTest extends TestCase {
+
+  private static final WKTReader reader = new WKTReader();
+
+  public static void main(String args[]) {
+    TestRunner.run(ConcaveHullExcavatorTest.class);
+  }
+
+  public ConcaveHullExcavatorTest(String name) {
+    super(name);
+  }
+
+  public void testSimple() throws ParseException {
+    checkHull(
+      "MULTIPOINT ((150 200), (200 160), (250 200), (200 190))",
+      0,
+      "POLYGON ((150 200, 200 160, 250 200, 200 190, 150 200))"
+    );
+  }
+
+  public void testPointsOnLine() throws ParseException {
+
+    Geometry input = read("MULTIPOINT((0 0), (0 1), (0 4))");
+    Geometry expected = new ConvexHull(input).getConvexHull();
+    Geometry actual = ConcaveHullExcavator.compute(input);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testThreeUniquePoints() throws ParseException {
+
+    Geometry input = read("MULTIPOINT((0 0), (0 1), (0 0), (1 1))");
+    Geometry expected = new ConvexHull(input).getConvexHull();
+    Geometry actual = ConcaveHullExcavator.compute(input);
+
+    assertEquals(expected, actual);
+  }
+
+
+  private void checkHull(String inputWKT, double tolerance, String expectedWKT) throws ParseException {
+    Geometry input = read(inputWKT);
+    Geometry expected = read(expectedWKT);
+    expected.normalize();
+    Geometry actual = ConcaveHullExcavator.compute(input, tolerance, 1.5);
+    actual.normalize();
+    checkEqual(expected, actual);
+  }
+
+  static WKTWriter wktWriter;
+  static {
+    wktWriter = new WKTWriter();
+    wktWriter.setOutputOrdinates(Ordinate.createXY());
+    wktWriter.setFormatted(true);
+    wktWriter.setMaxCoordinatesPerLine(3);
+    wktWriter.setTab(2);
+  }
+
+  public void testPoint1k_1() throws IOException, ParseException {
+
+    String pointsPath = TestFiles.getResourceFilePath("points-1k.wkt");
+    Geometry input = readFromFile(pointsPath);
+    String resultPath = TestFiles.getResourceFilePath("points-1k-hull.wkt");
+    Geometry expected = readFromFile(resultPath);
+
+    Geometry actual = ConcaveHullExcavator.compute(input);
+    compareGeometries(expected, actual, 0.0001);
+    assertTrue(actual.contains(input));
+  }
+
+  public void testPoint1k_2() throws IOException, ParseException {
+
+    String pointsPath = TestFiles.getResourceFilePath("points-1k.wkt");
+    Geometry input = readFromFile(pointsPath);
+    String resultPath = TestFiles.getResourceFilePath("points-1k-hull2.wkt");
+    Geometry expected = readFromFile(resultPath);
+
+    Geometry actual = ConcaveHullExcavator.compute(input, 0.01);
+    compareGeometries(expected, actual, 0.0001);
+    assertTrue(actual.contains(input));
+
+  }
+
+  private static void compareGeometries(Geometry expected, Geometry actual, double hsmDelta) {
+
+    // Normalize both
+    expected.normalize();
+    actual.normalize();
+
+    if (!expected.equalsExact(actual)) {
+      HausdorffSimilarityMeasure hsm = new HausdorffSimilarityMeasure();
+      double m = hsm.measure(expected, actual);
+      if (1 - m > hsmDelta) {
+        System.out.println("expected:\n" + wktWriter.writeFormatted(expected));
+        System.out.println("\n  actual:\n" + wktWriter.writeFormatted(actual));
+        System.out.println("\n  Hausdorff similarity:" + m);
+      }
+      assertEquals("Similarity of "+hsmDelta+" not reached: " + m, 1d, m, hsmDelta);
+    }
+
+  }
+
+  private static Geometry readFromFile(String path) throws IOException, ParseException {
+    WKTFileReader rdr = new WKTFileReader(path, new WKTReader());
+    rdr.setLimit(1);
+    List res = rdr.read();
+    return (Geometry) res.get(0);
+  }
+
+  private Geometry read(String wkt) throws ParseException {
+    return reader.read(wkt);
+  }
+
+  private static void checkEqual(Geometry expected, Geometry actual) {
+    Geometry actualNorm = actual.norm();
+    Geometry expectedNorm = expected.norm();
+    boolean equal = actualNorm.equalsExact(expectedNorm);
+    if (! equal) {
+      System.out.println("FAIL - Expected = " + expectedNorm
+        + " actual = " + actualNorm );
+    }
+    assertTrue(equal);
+  }
+
+}
